8db421edfaf0d3149b12e9d37479bb602d795751
chuck <407184508@qq.com>
2021-08-27 09:52:45 +0800

    注释演示代码

--- SBDemo/HomeViewController.swift
+++ SBDemo/HomeViewController.swift
@@ -17,20 +17,20 @@ class HomeViewController: UITableViewController {
         
     }
     
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-     
-        if identifier == ""  {
-            return false
-        }
-        
-        return true
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let controller = segue.destination as? LayoutViewController {
-            print("todo...\(controller.description)")
-        }
-    }
+//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//     
+//        if identifier == ""  {
+//            return false
+//        }
+//        
+//        return true
+//    }
+//    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let controller = segue.destination as? LayoutViewController {
+//            print("todo...\(controller.description)")
+//        }
+//    }
 
     @IBAction func unwindToHome(_ unwindSegue: UIStoryboardSegue) {
         if let hobbyController = unwindSegue.source as? HobbyViewController {
